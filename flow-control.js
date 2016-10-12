c2b2f97fc8986b3f0e6f57b44af141d799c7b1de
Flatiron School <ellis.paige.v@gmail.com>
2016-10-12 15:01:55 -0400

    Done.

--- flow-control.js
+++ flow-control.js
@@ -29,16 +29,26 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch (age) {
-    case 13:
-    case 14:
-    case 15:
-    case 16:
-    case 17:
-    case 18:
-    case 19:
+  var isTeenager = (age <= 19 && age >= 13)
+  switch (isTeenager) {
+    case true:
       return "You are a teenager";
     default:
       return "You have an age"
   }
 }
+
+// function switchAge(age) {
+//   switch (age) {
+//     case 13:
+//     case 14:
+//     case 15:
+//     case 16:
+//     case 17:
+//     case 18:
+//     case 19:
+//       return "You are a teenager";
+//     default:
+//       return "You have an age"
+//   }
+// }
